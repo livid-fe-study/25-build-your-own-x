65dba36fa640f129d50412e95c1b2133b8379c11
sasha1107 <sasha1107@naver.com>
2025-02-09 22:39:56 +0900

    feat: 실행 코드 위치 수정

--- sasha1107/main.js
+++ sasha1107/main.js
@@ -1,18 +1,19 @@
-import { Didact } from "./didact.js";
+import { Didact } from "./react/didact.js";
 
 /** @jsx Didact.createElement */
 function Counter() {
   const [state, setState] = Didact.useState(1);
-  return Didact.createElement(
-    "h1",
+  const h1 = Didact.createElement("h1", null, "Count: ", state);
+  const button = Didact.createElement(
+    "button",
     {
       onClick: () => {
         setState((prev) => prev + 1);
       },
     },
-    "Count: ",
-    state
+    "Click me"
   );
+  return Didact.createElement("div", null, h1, button);
 }
 const element = Didact.createElement(Counter, null);
 const container = document.getElementById("root");
